7b6339f07c80a3aabf90f692217062a6e03efed2
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-12-05 01:34:29 +0530

    add ton wallet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { TonConnectButton, TonConnectUIProvider } from '@tonconnect/ui-react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TonConnectUIProvider manifestUrl="https://test-youtube.s3.amazonaws.com/manifest.json">
+      <div className="App">
+     <TonConnectButton >TonConnectButton</TonConnectButton>  
+      </div>  </TonConnectUIProvider>
+
   );
 }
 
